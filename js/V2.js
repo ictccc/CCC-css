6b7867bc807da4f540ef80807f09dc4975d18faa
try4u <71308123+ictccc@users.noreply.github.com>
2020-09-29 15:40:51 +0900

    Add files via upload

--- js/V2.js
+++ js/V2.js
@@ -537,6 +537,41 @@ document.getElementById("download").onclick = (event) => {
     link.click();
 }
 
+//-----------------------------------------------------------------
+var file = document.getElementById('file');
+
+function loadLocalImage(e) {
+    var fileData = e.target.files[0];
+    if(!fileData.type.match('image.*')) {
+        alert('画像を選択してください');
+        return;
+    }
+
+    // FileReaderオブジェクトを使ってファイル読み込み
+    var reader = new FileReader();
+    // ファイル読み込みに成功したときの処理
+    reader.onload = function() {
+        // Canvas上に表示する
+        uploadImgSrc = reader.result;
+        canvasDraw();
+    }
+    // ファイル読み込みを実行
+    reader.readAsDataURL(fileData);
+}
+
+// ファイルが指定された時にloadLocalImage()を実行
+file.addEventListener('change', loadLocalImage, false);
+
+// Canvas上に画像を表示する
+function canvasDraw() {
+
+    // Canvas上に画像を表示
+    var img = new Image();
+    img.src = uploadImgSrc;
+    img.onload = function() {
+        CopyCtx.drawImage(img, 0, 0, 1000, 800);
+    }
+}
 
 ChangeCanvasSize();
 InitialDialog.showModal(); 
